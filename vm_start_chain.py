8e4440ec510acf6f00cde51ef0d9381119b7b0ae
Julie Deng <julieden@usc.edu>
2023-02-07 19:14:16 -0800

    Update vm_start_chain.py

--- vm_start_chain.py
+++ vm_start_chain.py
@@ -3,6 +3,14 @@ import socket
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
+    client.subscribe("julieden/pong")
+    client.message_callback_add("julieden/pong",on_message_from_pong)
+   
+def on_message(client, userdata, msg):
+    print("Default callback - topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
+
+def on_message_from_pong(client, userdata, message):
+    print("Custom callback  - PONG: "+message.payload.decode())
 
 if __name__ == '__main__':
     
@@ -20,8 +28,9 @@ if __name__ == '__main__':
       inp = input("Enter number: ")
       client.publish("julieden/ping",f"{inp}")
       print("Publishing number")
-      client.subscribe("julieden/pong",)
-      # print number
+      time.sleep(4)
+      client.on_message = on_message
+      client.connect(host="172.20.10.4", port=10000, keepalive=60)
       
       
 
