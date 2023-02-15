ce13199a7a9112f916321988e5cb3f35c81eb784
Julie Deng <julieden@usc.edu>
2023-02-14 16:31:07 -0800

    Update vm_start_chain.py

--- vm_start_chain.py
+++ vm_start_chain.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     client.loop_start()
     time.sleep(1)
     inp = input("Enter number: ")
-    inpt = inp-1
+    inp = inp-1
     while True:
       client.publish("julieden/ping",f"{inp+1}")
       print("Publishing number")
